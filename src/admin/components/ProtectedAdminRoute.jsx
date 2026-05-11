d75521f720332bbd21d8b9d28d80516f0bf758a6
Dumindu Sankalpa <kgdsankalpa00@gmail.com>
2026-05-12 01:32:35 +0530

    addmin access token repair

--- src/admin/components/ProtectedAdminRoute.jsx
+++ src/admin/components/ProtectedAdminRoute.jsx
@@ -1,6 +1,6 @@
 import { Navigate } from 'react-router-dom'
 
 export default function ProtectedAdminRoute({ children }) {
-  const token = localStorage.getItem('adminToken')
+  const token = localStorage.getItem('adminAccessToken')
   return token ? children : <Navigate to="/admin/login" replace />
 }
