d4d18b63d227706eecf083d2fa00a915dbdd2539
Dumindu Sankalpa <kgdsankalpa00@gmail.com>
2026-05-12 03:05:15 +0530

    upgrade katex and latex

--- src/components/MathText.jsx
+++ src/components/MathText.jsx
@@ -1,19 +1,34 @@
-import { InlineMath } from 'react-katex'
+import 'katex/dist/katex.min.css'
+import { InlineMath, BlockMath } from 'react-katex'
 
 export default function MathText({ text = '' }) {
-  const parts = text.split(/(\$[^$]+\$)/g)
+
+  // Split text into LaTeX and normal text
+  const parts = text.split(/(\$.*?\$)/g)
 
   return (
-    <span className="font-sinhala">
+    <>
       {parts.map((part, index) => {
+
+        // If wrapped with $
         if (part.startsWith('$') && part.endsWith('$')) {
+          const math = part.slice(1, -1)
+
           return (
-            <InlineMath key={index} math={part.slice(1, -1)} />
+            <InlineMath
+              key={index}
+              math={math}
+            />
           )
         }
 
-        return <span key={index}>{part}</span>
+        // Normal text
+        return (
+          <span key={index}>
+            {part}
+          </span>
+        )
       })}
-    </span>
+    </>
   )
 }
